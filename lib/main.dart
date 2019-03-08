ed502f80a53bf887f85eed62616f70f0ae325e2d
nram1234 <redanaser0@gmail.com>
2019-03-08 15:21:28 +0200

    last

--- lib/main.dart
+++ lib/main.dart
@@ -9,35 +9,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 void main() async{
 
-
+  List<Articles> i;
    var _list= await  getJson();
    if(_list!=null){
-     var i=_list['articles'] as List;
-     // print(_list );
-     print("ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
-     print(i[4] );
+     i= _list['articles'] as List;
+
    }else{
-     print("jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj");
+
    }
 
-  runApp(MyApp());
+  runApp(Home(i));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Home(),);}
-
-}
-
- Future getJson()  async{
-  //,headers: {"Accept":"application/json"}
-  String url="https://newsapi.org/v2/top-headlines?country=us&apiKey=791b5a001f314214814a5893ba725f8b";
-  var response = await http.get(url);
-
-
-
-  return json.decode(response.body);;
-}
--- lib/ui/Home.dart
+++ lib/ui/Home.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 
 
 class Home extends StatelessWidget {
+  List data;
+
+  Home(this.data) {}
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(home:   Scaffold(
+    return MaterialApp(home: Scaffold(
       appBar: AppBar(title: Text("Top News ",),
         actions: <Widget>[],
       ),
-      body: Text('jjjjjj'),
+      body: ListView.builder(
+          itemCount: data.length, itemBuilder: (BuildContext context, int i) {
+        return new Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(5.0)),
+          child: Row(children: <Widget>[Image.network(data[i]['urlToImage']),Text(data[i]['title'])],),);
+      }),
     ),);
-
-
-
-
   }
 }
